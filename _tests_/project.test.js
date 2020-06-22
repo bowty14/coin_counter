6412c54c7ba15b3140ee23eb839164bb8e0f05dd
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-06-22 14:42:03 -0700

    add 7th closure test for pennies.

--- _tests_/project.test.js
+++ _tests_/project.test.js
@@ -71,9 +71,15 @@ describe("CountCoins", () => {
     const amount = 0.10;
     expect(coinCounter.countCoins(amount, quarter, dime, nickel, penny)).toEqual("With this amount you would have 0 quarter(s), 1 dime(s), 0 nickel(s), 0 penny(ies).");
   });
+
   test("Should return 1 nickel", () => {
     const amount = 0.05;
     expect(coinCounter.countCoins(amount, quarter, dime, nickel, penny)).toEqual("With this amount you would have 0 quarter(s), 0 dime(s), 1 nickel(s), 0 penny(ies).");
+  });
+
+  test("Should return 1 penny", () => {
+    const amount = 0.01;
+    expect(coinCounter.countCoins(amount, quarter, dime, nickel, penny)).toEqual("With this amount you would have 0 quarter(s), 0 dime(s), 0 nickel(s), 1 penny(ies).");
   });
 });
 
